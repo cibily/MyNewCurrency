bd3c182451f3f6aa874ce768c87462f323272aa0
thepeg <marco.pegoraro@gmail.com>
2014-03-25 16:40:46 +0100

    add timeout to debug server

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(express.cookieParser());
 // prevent cache
 app.use(function(req, res, next){
 	res.setHeader('Last-Modified', (new Date()).toUTCString());
+	req.connection.setTimeout(500);
 	next();
 });
 
